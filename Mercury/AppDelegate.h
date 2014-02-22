1d66385edca94288fa9284bf6ce88f2b956886a4
Axel Rivera <axel@axelrivera.me>
2014-02-21 20:46:00 -0400

    Initial Commit

---
+++ Mercury/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mercury
+//
+//  Created by Axel Rivera on 2/21/14.
+//  Copyright (c) 2014 Axel Rivera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MercuryTests/MercuryTests.m
@@ -0,0 +1,34 @@
+//
+//  MercuryTests.m
+//  MercuryTests
+//
+//  Created by Axel Rivera on 2/21/14.
+//  Copyright (c) 2014 Axel Rivera. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MercuryTests : XCTestCase
+
+@end
+
+@implementation MercuryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
